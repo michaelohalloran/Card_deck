7f72e18be65c810fa4a9b87fcbd6760d94e7885a
MJ <kipsang20323@gmail.com>
2018-07-05 02:03:36 -0600

    add deal deck functionality to update UI

--- app.js
+++ app.js
@@ -1,101 +1,137 @@
-let output = document.getElementById("output");
-let startBtn = document.getElementById("startBtn");
-let howMany = document.getElementById("howMany");
-let cardDeck = [];
-let spadeImage = "<img src='spade.JPG'>";
-let heartImage = "<img src='heart.JPG'>";
-let diamondImage = "<img src='diamond.JPG'>";
-let clubImage = "<img src='club.JPG'>";
+var output = document.getElementById("output");
+var startBtn = document.getElementById("startBtn");
+
+//DECK
+const cardFace = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+const suits = ["clubs","spades","hearts","diams"];
+
+//CARD IMAGES
+var spadeImage = "<img src='spade.JPG'>";
+var heartImage = "<img src='heart.JPG'>";
+var diamondImage = "<img src='diamond.JPG'>";
+var clubImage = "<img src='club.JPG'>";
 
 
 //EVENT LISTENERS
-// startBtn.addEventListener("click", dealDeck);
-
-
+startBtn.addEventListener("click", startGame);
 
 //ARRAYS
-//playerArray
-//cardDeck array
+var players = [];
+var cardDeck = [];
 
 //FUNCTIONS/ACTIONS NEEDED
 
-//need to start the game
-//makeCardDeck, shuffle it, put players on display, 
-//give them each their cards, make UI empty to start to reset last game
+//startGame
+//makeCardDeck
+//shuffleDeck
+//dealDeck 
+//showCards
+//reset (make UI empty to start to reset last game)
 
 function startGame() {
+	//reset UI, clear any remnant values 
+	players = [];
+	cardDeck = []; 
+
+	//make playersArray 
+	var howManyPlayers = Number(document.getElementById("howManyPlayers").value);
+	makeCardDeck();
+	shuffleDeck(cardDeck);
+	dealDeck(howManyPlayers, cardDeck);
 
 }
 
-//need to build cards (4 suits of 13) and store in array
 
 function makeCardDeck() {
-	let nonNums = ["Jack","King","Queen","Ace"];
-	let Suits = ["Clubs","Spades","Hearts","Diamonds"];
-	//push 52 cards into array
-	//nested loop? 4 suits of 13 each
-	for(let i = 0; i < 4; i++) {
-		//make 4 suits
-		for (let j = 2; j < 15; j++) {
-			if(j < 11) {
-				cardDeck.push(`${j} ${Suits[i]}`);
-			} else {
-				//**************this won't work after j > 4
-				cardDeck.push(`${nonNums[j]} ${Suits[i]}`);
+
+	//nested loop of cardFace and Suits
+	//concatenate a Suit with a cardFace for each, push into large cardDeck
+	
+	//make 4 suits
+	// for(let i = 0; i < 4; i++) {
+	// 	//make 13 cards for each suit
+	// 	for (let j = 0; j < 13; j++) {
+	// 		cardDeck.push(`${cardFace[j]} ${suits[i]}`);
+	// 	}
+	// }
+	// return cardDeck;
+
+	for(var suit in suits) {
+		//using var causes bgColor to be undefined
+		var bgColor = (suits[suit] === "hearts" || suits[suit] === "diams") ? bgColor = "red": bgColor = "black";
+		var cardImg;
+		switch(suits[suit]) {
+			case "clubs":
+				cardImg = clubImage;
+				break;
+			case "spades":
+				cardImg = spadeImage;
+				break;
+			case "hearts": 
+				cardImg = heartImage;
+				break;
+			case "diams":
+				cardImg = diamondImage;
+				break;
+			default:
+				console.log('switch not working');
+		}
+		for(var c in cardFace) {
+			var card = {
+				suit: suits[suit],
+				num: cardFace[c],
+				bgColor: bgColor,
+				cardImg
 			}
+			cardDeck.push(card);
 		}
 	}
+	console.log('inside makeCardDeck, before shuffle');
+	console.log(cardDeck);
 	return cardDeck;
 }
 
 //need to shuffle cards randomly
 
-function randomShuffle() {
-	//%%%%%%%%%%%%need to always have 52 cards?
-
-	//make randomCard index
-	let randomCard = Math.floor(Math.random()*52);
-
+function shuffleDeck(cards) {
 	//loop through deck, randomizing by setting each item equal to 
 	//random position
 
-	for(let i = 0; i < cardDeck.length; i++) {
-		let temp = arr[i];
-		arr[i] = arr[randomCard];
-		arr[randomCard] = temp;
-	}
-	return cardDeck;
+	for(var i = cards.length -1; i > 0; i--) {
+		//make randomCard index
+		var randomCard = Math.floor(Math.random()*(i+1));
+		var temp = cards[i];
+		cards[i] = cards[randomCard];
+		cards[randomCard] = temp;
+		}
+	console.log('after shuffling, inside shuffle function');
+	console.log(cards);
+	return cards;
 }
 
+
 //need to see how many are playing and deal each a full deck of cards
+//Math.floor(array.length/numPlayers) =>each gets this many cards
 
-let numPlayers = Number(howMany.value);
+function dealDeck(numPlayers, deck) {
+	console.log(`There are ${numPlayers} playing`);
+	var numCardsPerPlayer = Math.floor(deck.length/numPlayers);
+	console.log(`Each player gets ${numCardsPerPlayer} cards`);
+	//this only prints 13 cards for 4 players, but need 4 sets of 13
+	for(let i = 0; i < numPlayers; i++) {
+		for(let j = 0; j < numCardsPerPlayer; j++) {
+			output.innerHTML += `
+			<ul style="color: ${deck[j].bgColor}">
+				<li>${deck[j].suit} ${deck[j].num} ${deck[j].cardImg}</li>
+			</ul>
+		`;
+		}
+		output.innerHTML += '<br/>'
+	}
+	
+}
 
 
-// function dealDeck() {
-	// alert(numPlayers);
-// 	switch: numPlayers {
-// 		case 1: 
-
-// 		break;
-
-// 		case 1: 
-
-// 		break;
-
-// 		case 1: 
-
-// 		break;
-
-// 		case 1: 
-
-// 		break;
-
-// 		case 1: 
-
-// 		break;
-
-// 		default: 
-// 			//do stuff
-// 	}
-// }
+//***********************************************************************
+//***********************************************************************
+//***********************************************************************
